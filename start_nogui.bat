dc3b39c5130b093d908b7916f5a835c85595f2c7
Molodos <michael-weichenrieder@t-online.de>
2021-01-10 14:33:42 +0100

    Start script fixes

--- start_nogui.bat
+++ start_nogui.bat
@@ -4,8 +4,7 @@ if not exist .\classes mkdir .\classes
 echo | set /p=Compiling class files...
 javac -encoding utf8 .\src\com\molodos\codingchallenge\*.java .\src\com\molodos\codingchallenge\models\*.java .\src\com\molodos\codingchallenge\displaydata\*.java -d .\classes
 echo done
-echo | set /p=Executing program...
-echo done
+echo Executing program...
 echo.
 echo.
 java -Dfile.encoding=UTF-8 -cp .\classes com.molodos.codingchallenge.ProblemSolver
--- start_nogui.sh
+++ start_nogui.sh
@@ -3,8 +3,7 @@ mkdir -p ./classes
 printf "Compiling class files..."
 javac -encoding utf8 ./src/com/molodos/codingchallenge/*.java ./src/com/molodos/codingchallenge/models/*.java ./src/com/molodos/codingchallenge/displaydata/*.java -d ./classes
 printf "done\n"
-printf "Executing program..."
-printf "done\n\n\n"
+printf "Executing program...\n\n\n"
 java -Dfile.encoding=UTF-8 -cp ./classes com.molodos.codingchallenge.ProblemSolver
 printf "Zum Beenden eine beliebige Taste drücken...\n"
-read
+read read
